415e2fb7ec63720ba40f3177e66fc929c5ffe50e
Ubuntu <ubuntu@ip-172-31-88-83.ec2.internal>
2020-09-29 14:37:34 +0000

    create spring boot app

---
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -0,0 +1,16 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication // for testing
+@EnableAutoConfiguration
+@Configuration
+public class PalTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalTrackerApplication.class, args);
+    }
+}
